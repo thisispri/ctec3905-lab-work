d5cf939737ca0b66a86118c4cb64b5e715465f72
thisispri <p2412923@my365.dmu.ac.uk>
2021-03-10 11:57:38 +0000

    edited the additem function

--- lab-06/todo.js
+++ lab-06/todo.js
@@ -1,8 +1,16 @@
 "use strict";
 
-function addItem(text) {
+function addItem(text, done) {
   const item = document.createElement('li');
-  item.textContent = text;
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  label.textContent = text;
+  input.type = "checkbox";
+  input.checked = done;
+  input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+  label.htmlFor = input.id;
+  item.appendChild(label);
+  item.appendChild(input);
   todo.appendChild(item);
 }
 add.addEventListener('click', ev => {
