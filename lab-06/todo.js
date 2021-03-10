2a25db4c16f5320b59e2f45d38ddb160e1b3bf96
thisispri <p2412923@my365.dmu.ac.uk>
2021-03-10 12:19:13 +0000

    .

--- lab-06/todo.js
+++ lab-06/todo.js
@@ -1,26 +1,35 @@
 "use strict";
 
 function addItem(text, done) {
-  const item = document.createElement('li');
-  const label = document.createElement('label');
-  const input = document.createElement('input');
-  label.textContent = text;
-  input.type = "checkbox";
-  input.checked = done;
-  input.id = `todo${todo.querySelectorAll('li').length + 1}`;
-  label.htmlFor = input.id;
-  item.appendChild(input);
-item.appendChild(label);
-  todo.appendChild(item);
-  const button = document.createElement('button');
-button.textContent = "×";
-button.addEventListener('click', ev => {
-  item.remove();
-});
-item.appendChild(button);
+	const item = document.createElement('li');
+	const label = document.createElement('label');
+	const input = document.createElement('input');
+	const button = document.createElement('button');
+	label.textContent = text;
+	input.type = "checkbox";
+	input.checked = done;
+	input.id = `todo${todo.querySelectorAll('li').length + 1}`;
+	input.addEventListener('input', ev => {
+		saveToStorage();
+	});
+	label.htmlFor = input.id;
+	button.textContent = "×";
+	button.addEventListener('click', ev => {
+		item.remove();
+		saveToStorage();
+	});
+	item.appendChild(input);
+	item.appendChild(label);
+	item.appendChild(button);
+	todo.appendChild(item);
 }
 add.addEventListener('click', ev => {
-  addItem(text.value);
+	if(text.value) {
+		addItem(text.value);
+		text.value = null;
+		text.focus();
+		saveToStorage(); // <-- this is a new line
+	}
 });
 
 function clearList() {
@@ -31,4 +40,37 @@ function clearList() {
 
 clear.addEventListener('click', ev => {
   clearList();
+});
+
+text.addEventListener('keydown', ev => {
+  if(ev.key == "Enter") {
+    add.click();
+  }
+});
+
+function saveToStorage() {
+	const elements = Array.from(todo.querySelectorAll('li'));
+	const data = elements.map(el => {
+		 return {
+       text: el.querySelector('label').textContent,
+			 done: el.querySelector('input').checked
+		 }
+	});
+	localStorage.setItem(todo.id, JSON.stringify(data));
+}
+function loadFromStorage() {
+	clearList();
+	const data = JSON.parse(localStorage.getItem(todo.id));
+	for (const item of data) {
+		addItem(item.text, item.done);
+	}
+}
+
+loadFromStorage();
+
+clear.addEventListener('click', ev => {
+  if(confirm("Are you sure you want to delete the entire list?")) {
+    clearList();
+    saveToStorage(); // <-- this is a new line
+  }
 });
